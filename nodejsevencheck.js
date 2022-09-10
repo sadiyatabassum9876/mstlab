e8ce2cb50d640718156d8235c61c6d224aa9a594
sadiyatabassum9876 <113251306+sadiyatabassum9876@users.noreply.github.com>
2022-09-10 21:14:04 +0530

    Add files via upload

---
+++ nodejsevencheck.js
@@ -0,0 +1,16 @@
+function even_odd(num)
+{
+	for(i=1;i<=num;i++)
+	{
+	   if(i%2==0)
+ 	   {
+	     console.log(i,"is even");
+	   }
+	   else
+	   {
+	     console.log(i,"is odd");
+	   }
+	}
+}
+
+even_odd(10);
